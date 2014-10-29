d4cacd5a06cfed430ea0286a830e1c00ca901239
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-10-29 11:43:44 -0400

    converts to new testing format

--- spec/speakToGrandma.spec.js
+++ spec/speakToGrandma.spec.js
@@ -1,12 +1,6 @@
 'use strict';
 
 describe('speakToGrandma', function() {
-
-  var speakToGrandma;
-  beforeEach(function() {
-    speakToGrandma = require('../lib/speakToGrandma');
-  });
-
   it('asks you to repeat yourself unless you shout', function() {
     expect(speakToGrandma('Hi Nana, how are you?')).toEqual('HUH?! SPEAK UP, SONNY!');
   });
